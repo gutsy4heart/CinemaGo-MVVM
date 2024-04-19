bf8e341dbf2bd48dba5c7b9f98f0458e0a4c9426
Nurma_kf03@itstep.edu.az <Nurma_kf03@itstep.edu.az>
2024-04-19 19:52:31 +0400

    Добавьте файлы проекта.

---
+++ CinamaGo/Model/Fields.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaGo.Model
+{
+    public class Fields
+    {
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Country { get; set; }
+        public string Producer { get; set; }
+        public string Actors { get; set; }
+        public string Description { get; set; }
+        public string Slogan { get; set; }
+        public string Genre { get; set; }
+        
+        public string Rating { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
